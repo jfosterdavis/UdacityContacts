eec6c24f788eb900c0d97bba1a11525a0147a8e1
Foster Davis <foster@breachbits.com>
2020-06-12 15:24:25 -0400

    added state and remove contact added a form that pulls on the state (controlled component) ready for filtering the contacts based on the form state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class ListContacts extends Component {
+	static PropTypes = {
+		contacts: PropTypes.array.isRequired,
+		onDeleteContact: PropTypes.func.isRequired,
+
+	}
+
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState(() => ({
+			query: query.trim()
+		}))
+	}
+
 	render() {
 		console.log('Props', this.props);
 
 		return (
+			<div className='list-contacts' >
+				<div className='list-contacts-top'>
+					<input
+						className={'search-contacts'}
+						type={'text'}
+						placeholder={'Search Contacts'}
+						value={this.state.query}
+						onChange={(event) => this.updateQuery(event.target.value)}
+						/>
+				</div>
 				<ol className='contact-list'>
 					{this.props.contacts.map((contact)=> (
 						<li key={contact.id} className={'contact-list-item'}>
@@ -19,12 +46,15 @@ class ListContacts extends Component {
 								<p>{contact.handle}</p>
 
 							</div>
-							<button className='contact-remove'>
+							<button
+								onClick={() => this.props.onDeleteContact(contact)}
+								className='contact-remove'>
 								Remove
 							</button>
 						</li>
 					))}
 				</ol>
+			</div>
 				)
 	}
 }
