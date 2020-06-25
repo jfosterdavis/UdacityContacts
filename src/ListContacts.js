7e7f422d1836a11e2adeb045237fa2ff08a350b5
Foster Davis <foster@breachbits.com>
2020-06-25 14:30:10 -0400

    step 3.12 adds an info field when user is searching, and gives a button to reset the query to show all contacts.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,10 @@ class ListContacts extends Component {
 			query: query.trim()
 		}))
 	}
+	
+	clearQuery = () => {
+		this.updateQuery('')
+	}
 
 	render() {
 		const { query } = this.state
@@ -40,6 +44,15 @@ class ListContacts extends Component {
 						onChange={(event) => this.updateQuery(event.target.value)}
 						/>
 				</div>
+				
+				{showingContacts.length !== contacts.length && (
+															   <div className='showing-contacts'>
+																<span>Now shoing {showingContacts.length} of {contacts.length}</span>
+																<button onClick={this.clearQuery}>Show all</button>
+															   </div>
+															   
+															   )}
+				
 				<ol className='contact-list'>
 					{showingContacts.map((contact)=> (
 						<li key={contact.id} className={'contact-list-item'}>
