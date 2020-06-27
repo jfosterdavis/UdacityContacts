7a3980c0eb2bc08c0eec880a442bf1755daaf6ab
Foster Davis <foster@breachbits.com>
2020-06-27 00:27:32 -0400

    instead of URL, the form now creates an object

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,20 @@
 import React, { Component} from 'react'
 import {Link} from "react-router-dom"
-import ImageInput from "./ImageInput";
+import ImageInput from "./ImageInput"
+import serializeForm from 'form-serialize'
 
 
 class CreateContact extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const values = serializeForm(e.target, {hash: true})
+        console.log('values', values)
+
+        if (this.props.onCreatecontact) {
+            this.props.onCreatecontact(values)
+        }
+    }
+
     render() {
         return (
             <div>
@@ -12,7 +23,7 @@ class CreateContact extends Component {
                     Close>
 
                 </Link>
-                <form className={'create-contact-form'}>
+                <form onSubmit={this.handleSubmit} className={'create-contact-form'}>
                     <ImageInput
                         className={'create-contact-avatar-input'}
                         name={'avatarURL'}
